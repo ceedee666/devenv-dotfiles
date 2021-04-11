56fae3d10b650570d345ba55c41aa47209179878
Christian Drumm <drumm@fh-aachen.de>
2021-04-11 16:23:55 +0200

    added aliases for tmux, pipenv and kubectl

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -4,5 +4,8 @@ alias mmv='noglob zmv -W'
 
 alias r='ranger'
 alias c='code'
+alias k='kubectl'
+alias tt='tmux a -t'
+alias ape='pipenv shell'
 
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
