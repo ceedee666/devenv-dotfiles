3de988584c03580f6d1df0f872f6d0b99778b311
Christian Drumm <christian.drumm@hey.com>
2022-06-16 13:20:53 +0200

    updated gitignore to not add .oh-my-zsh files

--- .oh-my-zsh/custom/keybindings.zsh
+++
@@ -1,5 +0,0 @@
- # Use vim keys in tab complete menu:
-bindkey -M menuselect 'h' vi-backward-char
-bindkey -M menuselect 'k' vi-up-line-or-history
-bindkey -M menuselect 'l' vi-forward-char
-bindkey -M menuselect 'j' vi-down-line-or-history
