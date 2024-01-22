a13dbb744611e800940a68e3826a034ec5229bfb
Christian Drumm <christian.drumm@hey.com>
2024-01-22 09:27:02 +0100

    disabled automatic closing of dap-ui

---
+++ .config/nvim/lua/plugins/nvim-dap.lua
@@ -0,0 +1,59 @@
+return {
+  {
+    "mfussenegger/nvim-dap",
+
+    dependencies = {
+
+      -- fancy UI for the debugger
+      {
+        "rcarriga/nvim-dap-ui",
+        -- stylua: ignore
+        keys = {
+          { "<leader>du", function() require("dapui").toggle({ }) end, desc = "Dap UI" },
+          { "<leader>de", function() require("dapui").eval() end, desc = "Eval", mode = {"n", "v"} },
+        },
+        opts = {},
+        config = function(_, opts)
+          -- setup dap config by VsCode launch.json file
+          -- require("dap.ext.vscode").load_launchjs()
+          local dap = require("dap")
+          local dapui = require("dapui")
+          dapui.setup(opts)
+          dap.listeners.after.event_initialized["dapui_config"] = function()
+            dapui.open({})
+          end
+        end,
+      },
+    },
+  },
+  {
+    "mfussenegger/nvim-dap-python",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+    },
+    ft = { "python" },
+    config = function()
+      local dap_python = require("dap-python")
+
+      local function find_debugpy_python_path()
+        -- Return the path to the debugpy python executable if it is
+        -- installed in $VIRTUAL_ENV, otherwise get it from Mason
+        if vim.env.VIRTUAL_ENV then
+          local paths = vim.fn.glob(vim.env.VIRTUAL_ENV .. "/**/debugpy", true, true)
+          if table.concat(paths, ", ") ~= "" then
+            return vim.env.VIRTUAL_ENV .. "/bin/python"
+          end
+        end
+
+        local mason_registry = require("mason-registry")
+        local path = mason_registry.get_package("debugpy"):get_install_path() .. "/venv/bin/python"
+        return path
+      end
+
+      local dap_python_path = find_debugpy_python_path()
+      vim.api.nvim_echo({ { "Using path for dap-python: " .. dap_python_path, "None" } }, false, {})
+
+      dap_python.setup(dap_python_path)
+    end,
+  },
+}
